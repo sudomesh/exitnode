258a7ad6625e9edb015ae10981384d360c9ef2e1
Marc Juul <juul@labitat.dk>
2013-08-29 02:53:05 -0700

    added intelligent url rewriter for captive portal faker

--- README.md
+++ README.md
@@ -10,11 +10,46 @@ Required packages so far:
   dnsmasq
   squid3
   apache2 (though any webserver should do)
-
+  postgresql (though any sql server should do)
+  python2.7
+  python-psycopg2
+  
 Assumed distro is debian.
+
+= Setup instructions =
+
+To /etc/postgresql/9.1/main/pg_hba.conf add this line:
+
+  host    captive         captive         127.0.0.1/32            md5
+
+Then:
+
+/etc/init.d/postgresql restart
+
+Then:
+
+su postgres
+psql
+create database captive;
+create table pass (id SERIAL, ipv4 varchar(16), ipv6 varchar(40), created timestamp DEFAULT current_timestamp);
+create user captive with password '?fakingthecaptive?';
+grant all privileges on database captive to captive;
+grant all privileges on table pass to captive;
+grant all privileges on sequence pass_id_seq to captive;
+
+Then:
+
+/etc/init.d/squid restart
+
 
 TODO:
 
 Set up tunneling between exit nodes and relay nodes (probably l2tp).
 
-Make the captive portal faker actually work.
+Automatically update firewall rules based on DNS every n minutes.
+
+Remove hardcoded values in rewrite.py
+
+Make the captive portal faker work with android and windows.
+
+Test captive portal faker on more devices.
---
+++ etc/squid3/rewrite.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2.7
+
+import psycopg2
+import re
+import sys
+
+DEBUG = False
+
+expiration = 24 # expiration in hours (you see the splash page again after this many hours)
+splash_url = "http://127.0.0.1/splash.html"
+splash_click_regex = "^http://click.splash.*"
+
+probe_regex_apple = "^http://www\.apple\.com/library/test/success\.html.*"
+splash_regex_apple = "^http://www.apple.com(/?)"
+
+conn = psycopg2.connect(host="127.0.0.1", database="captive", user="captive", password="?fakingthecaptive?")
+cur = conn.cursor()
+
+#clicked = False # TODO remove
+
+def debug(s):
+
+    if(DEBUG):
+        print "DEBUG: " + s
+
+if len(sys.argv) > 1:
+    if(sys.argv[1] == '-d'):
+        DEBUG = True
+        debug("Enabled debug output.")
+
+
+def did_user_already_click(ip):
+
+    cur.execute("SELECT ipv4, created FROM pass WHERE ipv4 = '"+ip+"';")
+    row = cur.fetchone()
+
+    if(row):
+        # TODO check expiration time
+        return True
+    else:
+        return False
+
+    return clicked
+
+def register_click(ip):
+
+    cur.execute("INSERT INTO pass (ipv4) VALUES (%s)", [ip])
+    conn.commit()
+    debug("Inserted ip: " + ip)
+
+
+while(True):
+    d = raw_input().split(' ')
+    url = d[0]
+
+    if(len(d) < 2):
+        print url # passthrough
+        continue
+
+    ip = d[1]
+    
+    if(re.match(probe_regex_apple, url)):
+
+        debug("apple probe from: " + ip)
+        
+        if(did_user_already_click(ip)):
+            
+            debug("user already clicked through. letting probe pass.")
+
+            print url
+
+        else:
+
+            debug("blocking probe")
+
+            print splash_url
+
+    elif(re.match(splash_regex_apple, url)):
+
+        debug("apple splash page fetch from: " + ip)
+
+        if(did_user_already_click(ip)):
+            
+            debug("user already clicked through. not showing splash page")
+
+            print url
+
+        else:
+
+            debug("showing splash page")
+
+            print splash_url
+
+    elif(re.match(splash_click_regex, url)):
+
+        debug("splash page clicked by: " + ip)
+
+        register_click(ip);
+
+        print splash_url
+
+    else:
+
+        print url
+
+conn.close()
+
+
