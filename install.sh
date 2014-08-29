125be316dd6bd86312b6ca88a5d9cc4c05cf46dc
wrought <mattsenate@gmail.com>
2014-08-29 16:04:51 -0700

    adds install script for one-liner installation

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/
+
+# install git
+apt-get upgrade && apt-get install git -y --force-yes
+
+# grab script
+git clone https://github.com/sudomesh/exitnode.git
+cd exitnode/
+
+# run script
+current_dir="$PWD"
+./provision.sh $current_dir
