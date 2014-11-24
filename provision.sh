9202b32502a6671b14a5b1275f5e984917a37107
Max B <maxb.personal@gmail.com>
2014-11-23 16:03:31 -0800

    Trying to get bmx6 working on exit node.

--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 MESH_IP=10.42.0.99
-MESH_MTU=1400
 ETH_IF=eth0
 PUBLIC_IP="$(ifconfig | grep -A 1 "$ETH_IF" | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)" 
 
@@ -43,23 +42,12 @@ apt-get update && apt-get install -y --force-yes \
   tmux \
   linux-headers-amd64
 
-mkdir /root/batman-build
-cd /root/batman-build
+cd /root
+git clone https://github.com/axn/bmx6.git bmx
+cd /root/bmx6
+make all && make install
 
-# batctl wasn't building, so I think for now we can build the version of batman-adv
-# that we like and we can use the current debian batctl package. I think it's ok 
-# if those two are different - maxb
-wget downloads.open-mesh.org/batman/stable/sources/batman-adv/batman-adv-2013.4.0.tar.gz
-# wget downloads.open-mesh.org/batman/stable/sources/batctl/batctl-2014.3.0.tar.gz
-tar -xvf batman-adv-2013.4.0.tar.gz 
-# tar -xvf batctl-2014.3.0.tar.gz
-cd batman-adv-2013.4.0
-make && make install
-# cd ../batctl-2014.3.0
-# make && make install
-apt-get install -y batctl
-
-REQUIRED_MODULES="nf_conntrack_netlink nf_conntrack nfnetlink l2tp_netlink l2tp_core l2tp_eth batman_adv"
+REQUIRED_MODULES="nf_conntrack_netlink nf_conntrack nfnetlink l2tp_netlink l2tp_core l2tp_eth"
 
 for module in $REQUIRED_MODULES
 do
@@ -76,45 +64,6 @@ done
 cp -r $SRC_DIR/src/etc/* /etc/
 cp -r $SRC_DIR/src/var/* /var/
 
-# Check if bat0 already has configs
-if grep -q "bat0" /etc/network/interfaces
-then
-  echo "bat0 already configured in /etc/network/interfaces"
-else
-  cat >>/etc/network/interfaces <<EOF
-
-# Batman interface
-# (1) Ugly hack to keep bat0 up: add a dummy tap device
-# (2) Add iptables rule to masquerade traffic from the mesh
-auto bat0
-iface bat0 inet static
-        pre-up ip tuntap add dev bat-tap mode tap
-        pre-up ip link set mtu 1400 dev bat-tap
-        pre-up ip link set bat-tap up
-        pre-up batctl if add bat-tap
-        post-up iptables -t nat -A POSTROUTING -s 10.0.0.0/8 ! -d 10.0.0.0/8 -j MASQUERADE
-        post-up iptables -t mangle -A POSTROUTING -s 10.0.0.0/8 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-        post-up iptables -t mangle -A POSTROUTING -d 10.0.0.0/8 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-        post-up ip link set mtu $MESH_MTU dev bat0
-        pre-down iptables -t nat -D POSTROUTING -s 10.0.0.0/8 ! -d 10.0.0.0/8 -j MASQUERADE
-        post-up iptables -t mangle -D POSTROUTING -s 10.0.0.0/8 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-        post-up iptables -t mangle -D POSTROUTING -d 10.0.0.0/8 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-        post-down batctl if del bat-tap
-        post-down ip link set bat-tap down
-        post-down ip tuntap del dev bat-tap mode tap
-        address $MESH_IP
-        netmask 255.0.0.0
-        mtu $MESH_MTU
-
-# Logical interface to manage adding tunnels to bat0
-iface mesh-tunnel inet manual
-        up ip link set \$IFACE up
-        post-up batctl if add \$IFACE
-        post-up ip link set mtu $MESH_MTU dev bat0
-        pre-down batctl if del \$IFACE
-        down ip link set \$IFACE down
-EOF
-fi
 
 pip install virtualenv
 
@@ -159,11 +108,6 @@ service tunneldigger start
 # source ~/.profile; 
 # nvm install 0.10; 
 # nvm use 0.10;
-
-
-# ssh stuffs
-# @@TODO: BETTER PASSWORD/Public Key
-# echo 'root:sudoer' | chpasswd
 
 # nginx stuffs
 echo "deb http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
