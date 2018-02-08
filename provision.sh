922cb8ada28d5f326e0abce55d9b5110a1384346
jhpoelen <jhpoelen@xs4all.nl>
2018-02-07 17:18:33 -0800

    attempt to get minimal broker to install on ubuntu

--- provision.sh
+++ provision.sh
@@ -7,13 +7,6 @@ ETH_IF=eth0
 PUBLIC_IP="45.34.140.42"
 PUBLIC_SUBNET="45.34.140.42/29"
 
-if [ "$#" -le 0 ]
-then
-  SRC_DIR="/vagrant/"
-else
-  SRC_DIR="$1"
-fi
-
 apt-get update && apt-get install -y --force-yes \
   build-essential \
   ca-certificates \
@@ -27,9 +20,6 @@ apt-get update && apt-get install -y --force-yes \
   openssl \
   perl \
   dnsmasq \
-  squid3 \
-  postgresql \
-  procps \
   procps \
   python-psycopg2 \
   python-software-properties \
@@ -42,8 +32,18 @@ apt-get update && apt-get install -y --force-yes \
   libevent-dev \
   ebtables \
   vim \
-  tmux \
-  linux-headers-amd64
+  tmux
+
+KERNEL_VERSION=$(uname -r)
+echo kernel version [$KERNEL_VERSION]
+
+apt-get install -y --force-yes \
+  cmake \
+  libnl-3-dev \
+  libnl-genl-3-dev \
+  build-essential \
+  pkg-config \
+  linux-image-extra-$(uname -r)
 
 mkdir ~/babel_build
 git clone https://github.com/sudomesh/babeld.git ~/babel_build/
@@ -64,34 +64,25 @@ do
   modprobe $module
 done
 
-# All exitnode file configs
-cp -r $SRC_DIR/src/etc/* /etc/
-chmod 755 /etc/squid3/*.py
-cp -r $SRC_DIR/src/var/* /var/
+pip install --upgrade pip
 
 pip install netfilter
 pip install virtualenv
 
-# rm -rf /opt/tunneldigger # ONLY NECESSARY IF WE WANT TO CLEAN UP LAST TUNNELDIGGER INSTALL
 git clone https://github.com/sudomesh/tunneldigger.git /opt/tunneldigger
 cd /opt/tunneldigger/broker
 virtualenv env_tunneldigger
 /opt/tunneldigger/broker/env_tunneldigger/bin/pip install -r requirements.txt
 
 
-# Check if babel tunnel is already configured 
-if grep -q "babeld" /opt/tunneldigger/broker/scripts/up_hook.sh
-then
-  echo "babel tunnel already configured in /opt/tunneldigger/broker/scripts/up_hook.sh"
-else
-  cat >/opt/tunneldigger/broker/scripts/up_hook.sh <<EOF
-  #!/bin/sh
-  ip link set \$3 up
-  ip addr add $MESH_IP/$MESH_PREFIX dev \$3
-  ip link set dev \$3 mtu \$4
-  babeld -a \$3
+cat >/opt/tunneldigger/broker/scripts/up_hook.sh <<EOF
+#!/bin/sh
+ip link set \$3 up
+ip addr add $MESH_IP/$MESH_PREFIX dev \$3
+ip link set dev \$3 mtu \$4
+babeld -a \$3
 EOF
-fi
+
 chmod 755 /opt/tunneldigger/broker/scripts/up_hook.sh
 
 cat >/etc/babeld.conf <<EOF
@@ -101,69 +92,23 @@ redistribute local ip $PUBLIC_SUBNET proto 0 deny
 redistribute local deny
 EOF
 
-cp $SRC_DIR/l2tp_broker.cfg /opt/tunneldigger/broker/l2tp_broker.cfg
+cp /opt/tunneldigger/broker/l2tp_broker.cfg.example /opt/tunneldigger/broker/l2tp_broker.cfg
 
 # Setup public ip in tunneldigger.cfg
-# Sorry this is so ugly - I'm not a very good bash programmer - maxb
 CFG="/opt/tunneldigger/broker/l2tp_broker.cfg"
 # This following like doesn't seem to be working!
 sed -i.bak "s#address=[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+#address=$PUBLIC_IP#" $CFG
 sed -i.bak "s#interface=lo#interface=$ETH_IF#" $CFG 
 
-# Setup address for gateway script
-sed -i.bak "s#MESHNET=[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/[0-9]\+#MESHNET=$MESHNET#" /etc/init.d/gateway
-
-
-# Use this line instead for wheezy
-# echo "host captive captive 127.0.0.1/32 md5" >> /etc/postgresql/9.1/main/pg_hba.conf 
-echo "host captive captive 127.0.0.1/32 md5" >> /etc/postgresql/9.4/main/pg_hba.conf 
-
-cp $SRC_DIR/setupcaptive.sql /home/
-cd /home
-/etc/init.d/postgresql restart; su postgres -c "psql -f setupcaptive.sql -d postgres"
-
-# @@TODO - Do we need to add these to startup?
 # adding init.d scripts to startup
-update-rc.d tunneldigger defaults
-# update-rc.d gateway defaults
-update-rc.d babeld defaults
-update-rc.d gateway
-update-rc.d captive_portal_redirect
+#update-rc.d tunneldigger defaults
+#update-rc.d babeld defaults
 
-service gateway start
-service captive_portal_redirect start
-service tunneldigger start
-# service gateway start
-service babeld start
-
-
-# Squid + redirect stuff for captive portal
-# /etc/init.d/squid restart
-# /etc/init.d/captive_portal_redirect start
-
-# node stuffs
-#cp $SRC_DIR/.profile ~/.profile
-# mkdir ~/nvm
-# cd ~/nvm
-# curl https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | bash
-# source ~/.profile; 
-# nvm install 0.10; 
-# nvm use 0.10;
-
-# uncomment for debian wheezy
-# nginx stuffs
-#echo "deb http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
-#echo "deb-src http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
-#apt-get update
-#apt-get install -y --force-yes nginx
-#cp $SRC_DIR/nginx.conf /etc/nginx/nginx.conf
-#update-rc.d nginx defaults
-#service nginx start
-
-rm -f /var/www/html/index*.html
+#service tunneldigger start
+#service babeld start
 
 # IP Forwarding
 sed -i.backup 's/\(.*net.ipv4.ip_forward.*\)/# Enable forwarding for mesh (altered by provisioning script)\nnet.ipv4.ip_forward=1/' /etc/sysctl.conf
 echo "1" > /proc/sys/net/ipv4/ip_forward
 
-shutdown -r now
+#reboot now
