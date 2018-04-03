671e9d5cfb53e697bf6ccc33e621dbff5b3ade6f
jhpoelen <jhpoelen@xs4all.nl>
2018-04-02 19:53:12 -0700

    simplify script; no date parsing needed because iso date string sort ok in alphabetical order already

--- src/opt/babeld-monitor/babeld-monitor.sh
+++ src/opt/babeld-monitor/babeld-monitor.sh
@@ -23,14 +23,14 @@ wait_for_babeld() {
 }
 
 
-date_last_error=$(sudo journalctl -u babeld -o short-iso | grep "Cannot allocate memory" | tail -n1 | awk '{print $1}')
+date_last_error=$(journalctl -u babeld -o short-iso | grep "Cannot allocate memory" | tail -n1 | awk '{print $1}')
 
-date_last_started=$(sudo journalctl -u babeld -o short-iso | grep "Started babeld" | tail -n1 | awk '{print $1}')
+date_last_started=$(journalctl -u babeld -o short-iso | grep "Started babeld" | tail -n1 | awk '{print $1}')
 
 if [[ "$date_last_error" == "" || "$date_last_started" == "" ]]; then
     echo "found no [Cannot allocate memory] error entry or babeld is not running"
 else
-  if [[$(date -d $date_last_error) > $(date -d $date_last_started)]]; then
+  if [ "$date_last_error" > "$date_last_started" ]; then
     echo "found [Cannot allocate memory] error since last babeld restart"
     echo "babeld restarting..."
     service babeld restart
@@ -41,6 +41,7 @@ else
     # add tunnel interfaces to babeld
     echo "add tunnel interfaces to babeld..."
     ip addr | tr ' ' '\n' | grep -E "l2tp[0-9]+$" | sort | uniq | xargs -L 1 babeld -a 
+    babeld -i | head -n1
     echo "add tunnel interfaces to babeld done."
   else
     echo "found no [Cannot allocate memory] error since last babeld restart"
